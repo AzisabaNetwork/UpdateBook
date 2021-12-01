8f585be028bd085c587099e9b1dcabfb7c9c11c9
46kuri_ <ghost.46kuri@gmail.com>
2021-12-01 14:56:49 +0900

    package名変更

---
+++ src/main/java/com/github/sirokuri_/updatebook/UpdateBook.java
@@ -0,0 +1,35 @@
+package com.github.sirokuri_.updatebook;
+
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.event.Listener;
+import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.scheduler.BukkitRunnable;
+
+public final class UpdateBook extends JavaPlugin implements Listener {
+
+    public BukkitRunnable task = null;
+
+    @Override
+    public void onEnable() {
+        Bukkit.getServer().getPluginManager().registerEvents(this, this);
+        Bukkit.getPluginManager().registerEvents(new BookOpen(this), this);
+        getCommand("update").setExecutor(new command(this));
+        saveDefaultConfig();
+        startTimer();
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+    private void startTimer() {
+        task = new BukkitRunnable() {
+            @Override
+            public void run() {
+                Bukkit.broadcastMessage(ChatColor.translateAlternateColorCodes('&',"---------------------\n&l鯖内パッチノートは\n&r&a/update open 1から5の数値 \n&fを入力すると見れます\n---------------------"));
+            }
+        };
+        task.runTaskTimer(this, 0, 6000);
+    }
+}
---
+++ src/main/java/com/github/sirokuri_/updatebook/command.java
@@ -0,0 +1,64 @@
+package com.github.sirokuri_.updatebook;
+
+import org.bukkit.*;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.BookMeta;
+import java.util.List;
+
+public class command implements CommandExecutor {
+
+    private final UpdateBook plugin;
+
+    public command(UpdateBook UpdateBook) {
+        this.plugin = UpdateBook;
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+        Player p = (Player) sender;
+        if (cmd.getName().equalsIgnoreCase("update")) {
+            if (args.length <= 0) {
+                return true;
+            }
+            if (args[0].equalsIgnoreCase("reload")) {
+                if (sender.hasPermission("Update.permission.admin")) {
+                    String WorldOnMessage = plugin.getConfig().getString("Update.WorldOnMessage");
+                    if (WorldOnMessage == null) return true;
+                    Bukkit.broadcastMessage(ChatColor.translateAlternateColorCodes('&', WorldOnMessage));
+                    plugin.reloadConfig();
+                }
+                return true;
+            }
+
+            if (args[0].equalsIgnoreCase("open")) {
+                if (args.length <= 1) {
+                    return true;
+                }
+                if (args[1].equalsIgnoreCase(args[1])) {
+                    for (String key : plugin.getConfig().getConfigurationSection("Update").getKeys(false)) {
+                        ItemStack itemStack = new ItemStack(Material.WRITTEN_BOOK);
+                        BookMeta bookMeta = (BookMeta) itemStack.getItemMeta();
+                        if (bookMeta == null) return true;
+                        List<String> Page = plugin.getConfig().getStringList("Update." + key + ".List");
+                        for (String P : Page) {
+                            bookMeta.addPage(ChatColor.translateAlternateColorCodes('&', P));
+                        }
+                        if (key.equalsIgnoreCase(args[1])) {
+                            bookMeta.setTitle("LIFE");
+                            bookMeta.setAuthor("46kuri_");
+                            itemStack.setItemMeta(bookMeta);
+                            p.openBook(itemStack);
+                        }
+                    }
+                    return true;
+                }
+                return true;
+            }
+        }
+        return true;
+    }
+}
